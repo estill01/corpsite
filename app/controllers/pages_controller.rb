5a6e8e22fdaaf5551c8143a955366f1ddd8a5b67
estill01 <ehs.stillman@gmail.com>
2011-01-27 15:33:33 -0500

    initial commit, basic structure

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  
+  def home
+    @title = "Boutique Technology Consulting"
+  end
+  
+  def contact
+  end
+  
+  def about
+  end
+    
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lowblink::Application.config.secret_token = 'f0f65875a1552f57fb081570206007fe9867048a224aeffbc14f0ab56e58d190a86a08303d9a028e2382036b3f530dbeac0a66a82ce5591de56f32505f622ff2'
