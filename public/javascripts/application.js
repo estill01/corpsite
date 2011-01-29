ed717ed009c37071b4a9c54d49d0efeb93e76b5f
estill01 <ehs.stillman@gmail.com>
2011-01-29 18:23:55 -0500

    image file clean up

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function () {
+	
+	// function cycle () {
+	// 	var x = true;
+	// 	while (x == true) {
+	// 		$(".data").delay(300).fadeOut(300);
+	// 		$(".web").delay(800).fadeIn(300);
+	// 		$(".web").delay(1100).fadeOut(300);			
+	// 		$(".data").delay(1400).fadeIn(300);
+	// 					
+	// 		// $(".web")
+	// 		// $(".data")							
+	// 	};
+	// };
+	// 
+	// cycle();
+	// 
+});
